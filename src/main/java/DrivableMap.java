93f437bfc14011415531d6191f9a5a2d1ba42c7e
Albert <albertyan2003@hotmail.com>
2022-09-18 02:32:04 -0400

    Innit yeah? Bop!

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,8 +27,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
+    public boolean addDrivable(String id, Drivable item) {
+        if (this.drivable_map.containsKey(id)) {
+            return false;
+        }
+        this.drivable_map.put(id, item);
+        return true;
+    }
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -37,17 +42,29 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable d : this.drivable_map.values()) {
+            if (d.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public ArrayList<Tradable> getTradable() {
+        ArrayList<Tradable> list = new ArrayList<Tradable>();
+        for (Drivable d : this.drivable_map.values()) {
+            if (d instanceof Tradable) {
+                list.add((Tradable) d);
+            }
+        }
+        return list;
+    }
 
 
-
-    
 }
